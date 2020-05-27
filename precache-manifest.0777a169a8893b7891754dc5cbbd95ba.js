e4df9bfd4a5c0df37ee10a6a073d8ab783f225c8
fatiherdogan <fatiherdogan_@windowslive.com>
2020-05-28 00:00:56 +0300

    Updates

--- precache-manifest.0777a169a8893b7891754dc5cbbd95ba.js
+++ precache-manifest.0777a169a8893b7891754dc5cbbd95ba.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "781dbfcdc2124f38668dd800df063b3e",
+    "revision": "0773c019f79a5388d04d63999a9b0f7a",
     "url": "/index.html"
   },
   {
-    "revision": "3857ef10cd7c3bcd2ff2",
+    "revision": "3ba93f220b142a628659",
     "url": "/static/css/main.9ccb609d.chunk.css"
   },
   {
-    "revision": "98c6ba47b744ea47e469",
-    "url": "/static/js/2.167246fd.chunk.js"
+    "revision": "c4a1ef25e820bd500477",
+    "url": "/static/js/2.41aaf58f.chunk.js"
   },
   {
     "revision": "5544b97da431ba1cb20d5aa93555fb4f",
-    "url": "/static/js/2.167246fd.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.41aaf58f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3857ef10cd7c3bcd2ff2",
-    "url": "/static/js/main.bd99d42e.chunk.js"
+    "revision": "3ba93f220b142a628659",
+    "url": "/static/js/main.a75d51c3.chunk.js"
   },
   {
     "revision": "2060b9a7e54a59ab7b07",
@@ -28,16 +28,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/github-pages-kullanimi.2963e72c.md"
   },
   {
-    "revision": "7be9722fe72d3068b259677d5714fbc4",
-    "url": "/static/media/github-pages-kullanimi.7be9722f.png"
-  },
-  {
     "revision": "0beab8a3e2c0a6a38e126e1b84ce87e6",
     "url": "/static/media/react-hooks-kullanimi.0beab8a3.md"
-  },
-  {
-    "revision": "514c33592d28181930b43d5f1a7680d6",
-    "url": "/static/media/react-hooks-kullanimi.514c3359.png"
   },
   {
     "revision": "7b270b768813d59f992d5843ba1d2e47",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c015a867704b56953da41c2c372f0da.js"
+  "/precache-manifest.0777a169a8893b7891754dc5cbbd95ba.js"
 );
 
 self.addEventListener('message', (event) => {
